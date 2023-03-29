93e67389c9ce16e3606d6f45f87b6c1e7c0511d1
Hari Harsha <hari33841@gmail.com>
2023-03-29 14:50:21 +0530

    **aNIMS

---
+++ knife bounce-aPpce/Assets/_GAME/_JC_Scripts/KnifePrefabScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class KnifePrefabScript : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Transform tower;
+    public ParticleSystem drops;
+
+
+    public MeshRenderer knife;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+        transform.position -= new Vector3(0.7f, 0, 0) * speed * Time.deltaTime;
+    }
+  
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Block") || other.gameObject.CompareTag("Finish"))
+        {
+            Instantiate(drops, other.transform.position+ new Vector3(1,0,-0.4f), Quaternion.Euler(0, -270, 0));
+            transform.position = new Vector3(-0.581f, transform.localPosition.y, transform.localPosition.z);
+            StartCoroutine(disable());
+            this.enabled = false;
+        }
+
+        if (other.gameObject.CompareTag("AfterBall"))
+        {
+            other.transform.SetParent(this.transform, true);
+            transform.DOJump(new Vector3(10, 10, 10), 5, 1, 4);
+            Destroy(this.gameObject, 0.5f);
+            Destroy(other.gameObject, 0.3f);
+        }
+    }
+
+    IEnumerator disable()
+    {
+        yield return new WaitForSeconds(1f);
+        //jelly.enabled = false;
+    }
+}
---
+++ knife bounce-aPpce/Assets/_GAME/_JC_Scripts/KnifeScript1.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class KnifeScript1 : MonoBehaviour
+{
+    private bool isTouch;
+
+    private bool isRelease = false;
+    public bool ischangecolor;
+    public float fireRate;
+    public GameObject[] knifemat;
+    [SerializeField] private float fireTime;
+    [SerializeField] private float nextfireRate;
+    int randcolors;
+    public Transform newBallPos;
+    public GameObject knife;
+    public GameObject PlayerRank;
+
+    public Image playerImg;
+    public float rankValue;
+
+    public GameObject countText;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ischangecolor = FindObjectOfType<ButtonManager>().changecolor;
+        transform.Rotate(0.5f, 0, 0);
+        if (Input.GetMouseButton(0))
+        {
+            Shooting();
+        }
+        else
+        {
+            StartCoroutine(txtDisable());
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                isTouch = true;
+ 
+            }
+         
+            if (touch.phase == TouchPhase.Ended)
+            {
+                StartCoroutine(txtDisable());
+                isTouch = false;
+            }
+
+            if (isTouch)
+            {
+
+                Shooting();
+            }
+        }
+    }
+
+    void Shooting()
+    {
+        countText.SetActive(true);
+        fireTime += Time.deltaTime;
+        nextfireRate = 1 / fireRate;
+
+        if (fireTime >= nextfireRate)
+        {
+            if (ischangecolor)
+            {
+                GameObject _knife =  Instantiate(knifemat[randcolors], transform.position, Quaternion.identity);
+                FindObjectOfType<FailScript>().Knifes.Add(_knife.gameObject.transform);
+
+                randcolors++;
+                newBallPos.transform.position += new Vector3(0, 0.7f, 0);
+                transform.position += new Vector3(0, 0.7f, 0);
+                transform.rotation = Quaternion.Euler(90, -180, 0);
+                playerImg.fillAmount += rankValue;
+                fireTime = 0;
+                if (randcolors >= knifemat.Length)
+                {
+                    randcolors = 0;
+                }
+            }
+            else
+            {
+                GameObject _knife =  Instantiate(knife, transform.position, Quaternion.identity);
+                FindObjectOfType<FailScript>().Knifes.Add(_knife.gameObject.transform);
+                newBallPos.transform.position += new Vector3(0, 0.7f, 0);
+                transform.position += new Vector3(0, 0.7f, 0);
+                transform.rotation = Quaternion.Euler(90, -180, 0);
+                playerImg.fillAmount += rankValue;
+                fireTime = 0;
+            }
+        }
+    }
+
+    IEnumerator txtDisable()
+    {
+        yield return new WaitForSeconds(0.5f);
+        countText.SetActive(false);
+    }
+}
